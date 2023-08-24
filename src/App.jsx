5eb772ca5f5de3aab3c1b83d064af32eb2d9c56a
Htoo Khant <vexixongaming02@gmail.com>
2023-08-24 08:38:36 +0630

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Routes,Route} from 'react-router-dom';
-import Product from './components/product';
+import Product from './components/Product';
 import './index.css'
 import Nav from './components/Nav';
 import AddToCart from './components/addToCart';
